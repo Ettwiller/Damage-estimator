7d00dc9a5a953130cb58e4b5b8663119a1d98361
ettwiller <ettwiller@neb.com>
2016-03-22 12:16:01 -0400

    adding mutation analysis

--- estimate_mutation.pl
+++ estimate_mutation.pl
@@ -3,12 +3,11 @@ use strict;
 use Getopt::Long qw(GetOptions);
 use File::Temp qw(tempfile);
 
-my $error_sentence = "USAGE : perl $0 --mpileup1 mileup_file_from_first_in_pair_read --mpileup2 mileup_file_from_second_in_pair_read --out damage_file_for_R --id library_name \nOPTIONAL :\n --qualityscore 35 (DEFAULT 30)\n --min_coverage_limit 10 (DEFAULT 1)\n --max_coverage_limit 500 (DEFAULT 100) --soft_masked 1 (DEFAULT 0)\n";
+my $error_sentence = "USAGE : perl $0 --mpileup1 mileup_file_from_first_in_pair_read --mpileup2 mileup_file_from_second_in_pair_read --out damage_file_for_R --id library_name \nOPTIONAL :\n --qualityscore 35 (DEFAULT 30)\n --min_coverage_limit 10 (DEFAULT 1)\n --max_coverage_limit 500 (DEFAULT 100) --soft_masked 1 (DEFAULT 0) > output_file \n";
 
 # declare the options upfront :
 my $file_R1;
 my $file_R2;
-my $out;
 my $generic;
 my $QUALITY_CUTOFF = 30;
 my $COV_MIN = 1;
@@ -18,7 +17,6 @@ my $SOFT_MASKED=0;
 GetOptions ("mpileup1=s" => \$file_R1,    # the mpileup file from the first in pair read
 	    "mpileup2=s" => \$file_R2, #the mpileup file from the second in pair read
 	    "qualityscore=s" => \$QUALITY_CUTOFF,#base quality (either direct from Illumina or recalibrated)
-	    "out=s" => \$out,#output file name.
 	    "id=s" => \$generic,
 	    "min_coverage_limit=s" => \$COV_MIN,
 	    "max_coverage_limit=s" => \$COV_MAX,
@@ -27,12 +25,8 @@ GetOptions ("mpileup1=s" => \$file_R1,    # the mpileup file from the first in p
 
 #=================================
 #if something went wrong in getting the option, notify :
-if (!$file_R1 || !$file_R2 || !$out || !$generic) {die $error_sentence}
+if (!$file_R1 || !$file_R2 || !$generic) {die $error_sentence}
 #=================================
-
-#do not override a file ==========
-if(-e $out) { die "File $out Exists, please remove old file or rename output file (--out)"};
-#================================= 
 
 
 my %RC;
@@ -66,37 +60,47 @@ $RC{'T_+'} = 'A_+';
 $RC{'C_+'} = 'G_+';
 $RC{'G_+'} = 'C_+';
 
-#start the main program =============================
+my %final;
 my $relative_count_R1 = get_relative_count($file_R1);
 my $relative_count_R2 = get_relative_count($file_R2);
-#====================================================
 
-#open the output file ====================================
-open(OUT, ">$out") or die "can't save result into $out\n";
-#=========================================================  
-
-
-#go over all the mutation possibilities :
-foreach my $type (keys %RC)
-
+foreach my $type (keys %$relative_count_R1)
 {
-    my $ref = $$relative_count_R1{$type};
-    foreach my $position (sort {$a<=>$b} keys %$ref)
+    my $type_RC = $RC{$type};
+    my $couple = join("-", sort($type,$type_RC));
+    
+    
+    my $total_type1 = $$relative_count_R1{$type}{"total_type"};
+    my $total_type2 = $$relative_count_R2{$type_RC}{"total_type"};
+    
+    my $total1 = $$relative_count_R1{$type}{"total"};
+    my $total2 = $$relative_count_R2{$type_RC}{"total"};
+    
+    if ($$relative_count_R1{$type}{"total_type"} && $$relative_count_R2{$type}{"total_type"})
     {
-	my $value1 = $$relative_count_R1{$type}{$position}{"relative"};
-	my $value2 = $$relative_count_R2{$type}{$position}{"relative"};
-	my $abs1 = $$relative_count_R1{$type}{$position}{"absolute"};
-	my $abs2 = $$relative_count_R2{$type}{$position}{"absolute"};
-	if ($$relative_count_R1{$type}{$position}{"relative"} && $$relative_count_R2{$type}{$position}{"relative"})
-	{
-	   
+	my $total_type = $total_type1 + $total_type2;
+	my $total = $total1 + $total2;
+	my $value = $total_type / $total;
+	#rank    n       type    experiment      count   family
+	my $line = "$total_type\t$type\t$generic\t$value\t$couple";
+	
+	$final{$type}{"value"}=$value;
+	$final{$type}{"line"}=$line;
+    }
 
-	    print OUT "$generic\t$type\tR1\t$value1\t$abs1\t$position\n";
-	    print OUT "$generic\t$type\tR2\t$value2\t$abs2\t$position\n";
-	}
+}
+
+foreach my $type (keys %final)
+{
+    my $type_RC = $RC{$type};
+    if ($final{$type} && $final{$type_RC})
+    {
+	my $ratio1 = $final{$type}{"value"} / $final{$type_RC}{"value"};
+	my $line = $final{$type}{"line"};
+	print "$line\t$ratio1\n";
     }
 }
-close OUT;
+
 
 
 
@@ -170,9 +174,10 @@ sub get_relative_count {
 
 	if ($SOFT_MASKED ==0)
 	{
-	    #if we do not want to solf masked the repeat : all reference becomes UPPER CASE. 
+	        #if we do not want to solf masked the repeat : all reference becomes UPPER CASE. 
 	    $ref = uc($ref);
 	}
+
 	if ($number == $length_mutation && $number >= $COV_MIN && $number <= $COV_MAX && $ref =~/[ACTG]/)
 	    #$number == $length_mutation : making sure that the position array and base array are in agreement. 
 	    # $number > 0  : position with at least one read
@@ -188,7 +193,6 @@ sub get_relative_count {
 		#given a character $q, the corresponding Phred quality can be calculated with:                                             
                 #$Q = ord($q) - 33;  
 		my $quality_score = ord($qualities[$i]) - 33;
-		
 		if ($quality_score > $QUALITY_CUTOFF)
 		{
 		    
@@ -196,21 +200,21 @@ sub get_relative_count {
 		    my $rp = $poss[$i];
 		    if($ch=~/[ATCG]/){
 			my $type = $ref."_".$ch;
-			$result{"type"}{$rp}{$type}++;
-			$result{"nt"}{$rp}{$ref}++;
+			$result{"type"}{$type}++;
+			$result{"nt"}{$ref}++;
 			
 		    }
 		    elsif($ch=~/[\+\-]/ && $rp > 5){ #this is to remove the artefact of alignment algorithm that add indels (?) at the begining. Should be delt differently. 
                         my $type = $ref."_".$ch;
-                        $result{"type"}{$rp}{$type}++;
-                        $result{"nt"}{$rp}{$ref}++;
+                        $result{"type"}{$type}++;
+                        $result{"nt"}{$ref}++;
 
                     }
 
 		    elsif($ch eq "."){
 			my $type = $ref."_".$ref;
-			$result{"type"}{$rp}{$type}++;
-			$result{"nt"}{$rp}{$ref}++;
+			$result{"type"}{$type}++;
+			$result{"nt"}{$ref}++;
 			
 		    }
 		}
@@ -219,34 +223,19 @@ sub get_relative_count {
 	}
     }
     close FILE;
-    my $positions = $result{"type"};
-    foreach my $pos (keys %$positions)
+    my $mutations = $result{"type"};
+    
+    foreach my $type (keys %$mutations)
     {
-	my $mutations = $$positions{$pos};
-	foreach my $type (keys %$mutations)
-	{
-	    my $total_type = $result{"type"}{$pos}{$type};
-	    $type =~/(.)\_(.)/;
-	    my $total = $result{"nt"}{$pos}{$1};
-	    
-	    my $value = $total_type/$total;
-	    
-	    $final{$type}{$pos}{"relative"}=$value;
-	    $final{$type}{$pos}{"absolute"}=$total_type;
-	}
-    }
- 
+	my $total_type = $result{"type"}{$type};
+	$type =~/(.)\_(.)/;
+	my $total = $result{"nt"}{$1};
+	
+	$final{$type}{"total_type"}=$total_type;
+	$final{$type}{"total"}=$total;
+	
+    } 
     return(\%final);
 }
 
 
-sub reverse_complement {
-        my $dna = shift;
-
-	# reverse the DNA sequence
-        my $revcomp = reverse($dna);
-
-	# complement the reversed DNA sequence
-        $revcomp =~ tr/ACGTacgt/TGCATGCA/;
-        return $revcomp;
-}

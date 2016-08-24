da41cd8fa84e0b4de2251b893b945b7c54c24fcf
ettwiller <ettwiller@neb.com>
2016-08-24 16:05:08 -0400

    fixing plot_damage.R

--- plot_damage.R
+++ plot_damage.R
@@ -25,9 +25,11 @@ new_mutation$type <- factor(new_mutation$type, level=typ)
 local_color <- c("cornflowerblue", "royalblue4","grey1", "grey10","grey20", "grey30", "grey40", "grey50", "grey60", "grey70", "grey80", "grey90", "grey100")
 
 d<-ggplot(new_mutation, aes(x = reorder(type, damage), y = log2(damage), color=experiment)) + 
+ #geom_point(position=position_jitter(height = 0.1), alpha = 0.6, size=1.5) +
   geom_point( alpha = 0.6, size=1.5) +    
     scale_colour_manual(values = local_color) +
   geom_hline(yintercept= log2(1.5), color = "#990000", linetype="dashed") +
+annotate("text", x= 3, y = log2(1.6), label = "Above this line one third of variant = damage",  color = "#990000") +
  geom_hline(yintercept= 0, color = "grey") +	
   theme(panel.background = element_rect(fill = 'white', colour = 'white'), 
         legend.position="bottom", axis.text.x = element_text(angle = 90, hjust = 1, size=11)) +

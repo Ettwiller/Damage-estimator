4d5c16b5dc2343d99aebe519d01c84bc8b424fe3
ettwiller <ettwiller@neb.com>
2016-08-24 15:45:04 -0400

    fixing plot_damage.R

--- plot_damage.R
+++ plot_damage.R
@@ -24,15 +24,15 @@ new_mutation$type <- factor(new_mutation$type, level=typ)
 #coloring scheme (feel free to change)
 local_color <- c("cornflowerblue", "royalblue4","grey1", "grey10","grey20", "grey30", "grey40", "grey50", "grey60", "grey70", "grey80", "grey90", "grey100")
 
-d<-ggplot(new_mutation, aes(x = reorder(type, damage), y = log2(damage), color=type)) + 
+d<-ggplot(new_mutation, aes(x = reorder(type, damage), y = log2(damage), color=experiment)) + 
   geom_point(position=position_jitter(height = 0.25), alpha = 0.6, size=1.5) +    
     scale_colour_manual(values = local_color) +
   geom_hline(yintercept= log2(1.5), color = "#990000", linetype="dashed") +
  geom_hline(yintercept= 0, color = "grey") +	
   theme(panel.background = element_rect(fill = 'white', colour = 'white'), 
-        legend.position="none", axis.text.x = element_text(angle = 90, hjust = 1, size=11)) +
+        legend.position="bottom", axis.text.x = element_text(angle = 90, hjust = 1, size=11)) +
   #ylim(0, 1) +
-  ggtitle("GIV scores")
+  ggtitle("GIV scores in accending order")
 ggsave(args[2], d, width=9, height=6) 
 
 

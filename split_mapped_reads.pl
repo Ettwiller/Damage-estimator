472e14a05b37ffa1bcffefcdab59b399ae60ecc4
Tobias Meissner <meissner.t@googlemail.com>
2017-03-28 09:31:08 -0500

    downsampling to fixed number of reads
    Added an option (-sample) to downsample to a fixed number of reads (default 5M). This will require sambamba (http://lomereiter.github.io/sambamba/) as an additional dependency. Also replaced samtools mpileup with sambamba mpileup for speed gain

--- split_mapped_reads.pl
+++ split_mapped_reads.pl
@@ -3,7 +3,7 @@ use strict;
 use Getopt::Long qw(GetOptions);
 use File::Temp qw(tempfile);
 
-my $error_sentence = "USAGE : perl $0 --bam bam_file -genome genome_file.fasta -mpileup1 output_file1 -mpileup2 output_file2 -Q 10 (DEFAULT 0) -q 20 (DEFAULT 10)";
+my $error_sentence = "USAGE : perl $0 --bam bam_file -genome genome_file.fasta -mpileup1 output_file1 -mpileup2 output_file2 -Q 10 (DEFAULT 0) -q 20 (DEFAULT 10) -sample 5000000 (DEFAULT 0)";
 
 # declare the options upfront :
 my $BAM;
@@ -12,13 +12,15 @@ my $q = 10;
 my $mpileupR1;
 my $mpileupR2;
 my $genome;
+my $sample = 0;
 #get options :
 GetOptions ("bam=s" => \$BAM,    # the mpileup file from the first in pair read
 	    "Q=s" => \$Q,
 	    "q=s" => \$q,
 	    "mpileup1=s" => \$mpileupR1,
 	    "mpileup2=s" => \$mpileupR2,
-	    "genome=s" => \$genome
+	    "genome=s" => \$genome,
+	    "sample=s" => \$sample
     ) or die $error_sentence;
 
 #=================================
@@ -26,7 +28,8 @@ GetOptions ("bam=s" => \$BAM,    # the mpileup file from the first in pair read
 if (!$BAM || !$mpileupR1 || !$mpileupR2 || !$genome) {die $error_sentence}
 #=================================
 
-#create tmp files for bam1 and bam2 ===================
+#create tmp files for samle and bam1 and bam2 ===================
+my $bam_sample = new File::Temp( UNLINK => 1 );
 my $bam_R1 = new File::Temp( UNLINK => 1 );
 my $bam_R2 = new File::Temp( UNLINK => 1 );
 
@@ -34,16 +37,38 @@ my $bam_R2 = new File::Temp( UNLINK => 1 );
 #if(-e $out) { die "File $out Exists, please remove old file or rename output file (--out)"};
 #================================= 
 
-my $command_R1 = "samtools view -b -f 64 -b $BAM > $bam_R1";    
-my $command_R2 = "samtools view -b -f 128 -b $BAM > $bam_R2";
+# downsample if sample size is given
+if ($sample > 0) {
+  my @reads = `samtools idxstats $BAM | cut -f3`;
+  my $creads = 0;
+  foreach (@reads){
+    $creads += $_;
+  }
+  my $frac = $sample / $creads;
+  my $subsample = "sambamba view -f bam -t 32 --subsampling-seed=3 -s $frac $BAM -o $bam_sample.bam";
+  
+  my $command_R1 = "samtools view -f 64 -b $bam_sample.bam > $bam_R1";    
+  my $command_R2 = "samtools view -f 128 -b $bam_sample.bam > $bam_R2";
 
-    #run mpileup on the separated Bam files.
+  #run mpileup on the separated Bam files.
+  my $command_mplipeup_R1 = "sambamba mpileup -o $mpileupR1 $bam_R1 --samtools -O -s -q $q -Q $Q -f $genome";
+  my $command_mplipeup_R2 = "sambamba mpileup -o $mpileupR2 $bam_R2 --samtools -O -s -q $q -Q $Q -f $genome";
 
-my $command_mplipeup_R1 = "samtools mpileup -O -s -q $q -Q $Q -f $genome $bam_R1 > $mpileupR1";
-my $command_mplipeup_R2 = "samtools mpileup -O -s -q $q -Q $Q -f $genome $bam_R2 > $mpileupR2";
+  system($subsample);
+  system($command_R1);
+  system($command_R2);
+  system($command_mplipeup_R1);
+  system($command_mplipeup_R2);
+} else {
+  my $command_R1 = "samtools view -f 64 -b $BAM > $bam_R1";
+  my $command_R2 = "samtools view -f 128 -b $BAM > $bam_R2";
 
-system($command_R1);
-system($command_R2);
-system($command_mplipeup_R1);
-system($command_mplipeup_R2);
+  #run mpileup on the separated Bam files.
+  my $command_mplipeup_R1 = "sambamba mpileup -o $mpileupR1 $bam_R1 --samtools -O -s -q $q -Q $Q -f $genome";
+  my $command_mplipeup_R2 = "sambamba mpileup -o $mpileupR2 $bam_R2 --samtools -O -s -q $q -Q $Q -f $genome";
 
+  system($command_R1);
+  system($command_R2);
+  system($command_mplipeup_R1);
+  system($command_mplipeup_R2);
+}
